5231cac31a9d6d922358369652d7457718c21d47
DinoZF <907356745@qq.com>
2019-10-13 23:51:52 +0800

    push

--- lib/demo/post_show.dart
+++ lib/demo/post_show.dart
@@ -15,7 +15,26 @@ class PostShow extends StatelessWidget {
         title: Text('${post.title}'),
         elevation: 0.0,
       ),
-      
+      body: Column(
+        children: <Widget>[
+          Image.network(
+            post.imageUrl,
+          ),
+          Container(
+            padding: EdgeInsets.all(32.0),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('${post.title}', style: Theme.of(context).textTheme.title),
+                Text('${post.author}', style: Theme.of(context).textTheme.subtitle),
+                SizedBox(height: 32.0,),
+                Text('${post.description}', style: Theme.of(context).textTheme.body1),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/model/post.dart
+++ lib/model/post.dart
@@ -1,13 +1,15 @@
 class Post {
-  final String title;
-  final String author;
-  final String imageUrl;
-
   const Post({
     this.title,
     this.author,
-    this.imageUrl
-  });  
+    this.imageUrl,
+    this.description
+  }); 
+
+  final String title;
+  final String author;
+  final String imageUrl;
+  final String description;
 }
 
 final List<Post> posts = [
@@ -15,75 +17,92 @@ final List<Post> posts = [
     title: 'Candy Shop',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/candy-shop.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Childhood in a picture',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/childhood-in-a-picture.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Contained',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/contained.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
+
   ),
   Post(
     title: 'Dragon',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/dragon.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
+
   ),
   Post(
     title: 'Free Hugs',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/free_hugs.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Gravity Falls',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/gravity-falls.png',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Icecream Truck',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/icecreamtruck.png',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'keyclack',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/keyclack.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Overkill',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/overkill.png',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Say Hello to Barry',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/say-hello-to-barry.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Space Skull',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/space-skull.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'The Old Fashioned',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/the-old-fashioned.png',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Tornado',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/tornado.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'Undo',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/undo.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   ),
   Post(
     title: 'White Dragon',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/white-dragon.jpg',
+    description: 'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real! Dream what you want to dream;go where you want to go;be what you want to be,because you have only one life and one chance to do all the things you want to do.'
   )
 ];
